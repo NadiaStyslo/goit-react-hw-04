21f08fba68681656a70ee7f6d5f8573cea8637d0
NadiaStyslo <sir.ihor@gmail.com>
2024-02-09 11:35:01 -0500

    finished

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,16 @@
-import css from './Loader.module.css'
-import {FallingLines} from 'react-loader-spinner'
+import css from './Loader.module.css';
+import { FallingLines } from 'react-loader-spinner';
 const Loadear = () => {
-    return (
-         <FallingLines
-  color="#ed4b4b"
-  width="100"
-  visible={true}
-  ariaLabel="falling-circles-loading"
-  />)
-     
-}
+  return (
+    <div className={css.loader}>
+      <FallingLines
+        color="#ed4b4b"
+        width="100"
+        visible={true}
+        ariaLabel="falling-circles-loading"
+      />
+    </div>
+  );
+};
 
-export default Loadear
+export default Loadear;
